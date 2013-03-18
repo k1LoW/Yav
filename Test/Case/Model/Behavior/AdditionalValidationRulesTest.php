41a07d1dc46a01de254cdc42ae5eb83263801647
k1LoW <k1lowxb@gmail.com>
2013-03-18 17:30:23 +0900

    Indent

--- Test/Case/Model/Behavior/AdditionalValidationRulesTest.php
+++ Test/Case/Model/Behavior/AdditionalValidationRulesTest.php
@@ -4,14 +4,20 @@ class YavPost extends CakeTestModel {
 
     public $name = 'YavPost';
     public $actsAs = array(
-                           'Cakeplus.AddValidationRule',
-                           'Yav.AdditionalValidationRules'
-                           );
+        'Cakeplus.AddValidationRule',
+        'Yav.AdditionalValidationRules'
+    );
 
     public $validate = array(
-                             'not_empty_with1' => array('notEmptyWith' => array('rule' => array('notEmptyWith', array('not_empty_with2', 'not_empty_with3')))),
-                             'katakana_and_space' => array('katakanaAndSpace' => array('rule' => array('katakanaAndSpace'))),
-                             );
+        'not_empty_with1' => array(
+            'notEmptyWith' => array(
+                'rule' => array('notEmptyWith', array('not_empty_with2', 'not_empty_with3'))
+            )),
+        'katakana_and_space' => array(
+            'katakanaAndSpace' => array(
+                'rule' => array('katakanaAndSpace')
+            )),
+    );
 }
 
 class AdditionalValidationRulesTest extends CakeTestCase {
@@ -40,22 +46,22 @@ class AdditionalValidationRulesTest extends CakeTestCase {
      */
     public function test_notEmptyWith(){
         $data = array(
-                'YavPost' => array(
-                                   'title' => 'タイトル',
-                                   'not_empty_with1' => '',
-                    ),
-                );
+            'YavPost' => array(
+                'title' => 'タイトル',
+                'not_empty_with1' => '',
+            ),
+        );
         $this->assertIdentical( $this->YavPost->create( $data ) , $data);
         $this->YavPost->validates();
         $this->assertFalse( array_key_exists('not_empty_with1' , $this->YavPost->validationErrors ) );
 
         $data = array(
-                'YavPost' => array(
-                                   'title' => 'タイトル',
-                                   'not_empty_with1' => '',
-                                   'not_empty_with3' => '空でない',
-                    ),
-                );
+            'YavPost' => array(
+                'title' => 'タイトル',
+                'not_empty_with1' => '',
+                'not_empty_with3' => '空でない',
+            ),
+        );
         $this->assertIdentical( $this->YavPost->create( $data ) , $data);
         $this->YavPost->validates();
         $this->assertTrue( array_key_exists('not_empty_with1' , $this->YavPost->validationErrors ) );
@@ -67,19 +73,19 @@ class AdditionalValidationRulesTest extends CakeTestCase {
      */
     public function test_katakanaAndSpace(){
         $data = array(
-                'YavPost' => array(
-                    'katakana_and_space'  =>  'hiragana',
-                    ),
-                );
+            'YavPost' => array(
+                'katakana_and_space'  =>  'hiragana',
+            ),
+        );
         $this->assertIdentical( $this->YavPost->create( $data ) , $data);
         $this->YavPost->validates();
         $this->assertTrue( array_key_exists('katakana_and_space' , $this->YavPost->validationErrors ) );
 
         $data = array(
-                'YavPost' => array(
-                    'katakana_and_space'  =>  'カタカナ　ト　スペース',
-                    ),
-                );
+            'YavPost' => array(
+                'katakana_and_space'  =>  'カタカナ　ト　スペース',
+            ),
+        );
         $this->assertIdentical( $this->YavPost->create( $data ) , $data);
         $this->YavPost->validates();
         $this->assertFalse( array_key_exists('katakana_and_space' , $this->YavPost->validationErrors ) );
